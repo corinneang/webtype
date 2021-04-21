af8648c149036f2d51dc2267953b557a63455f92
corinneang <cang@risd.edu>
2021-04-21 12:24:26 -0400

    east

--- script_winder.js
+++ script_winder.js
@@ -1,6 +1,9 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 
+const wghtmax = 900;
+
+
 openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = document.querySelector(button.dataset.modalTarget)
@@ -55,6 +58,22 @@ function closeModal(modal) {
 
 
 
+	//mouseposition
+	$('#garden').mousemove(function(event) {
+
+		console.log(event.pageY);
+
+		let cursorY = 1 - (event.pageY) / $(this).height();
+
+		let settingY = Math.floor(cursorY * wghtmax);
+
+console.log(settingY)
+
+		$(".nature").css({
+			"--wght": settingY
+		});
+		
+	});
 
 
     	//click buttons
@@ -78,3 +97,36 @@ function closeModal(modal) {
 			"--wght": 900
 		});	
 	});
+
+	// collect all the divs
+var divs = document.getElementsByTagName('img');
+// get window width and height
+var winWidth = window.innerWidth;
+var winHeight = window.innerHeight;
+
+// i stands for "index". you could also call this banana or haircut. it's a variable
+for ( var i=0; i < divs.length; i++ ) {
+ 	
+    // shortcut! the current div in the list
+    var thisDiv = divs[i];
+    
+    // get random numbers for each element
+    randomTop = getRandomNumber(0, winHeight);
+    randomLeft = getRandomNumber(0, winWidth);
+    
+    // update top and left position
+    thisDiv.style.top = randomTop +"px";
+    thisDiv.style.left = randomLeft +"px";
+    
+}
+
+// function that returns a random number between a min and max
+function getRandomNumber(min, max) {
+    
+  return Math.random() * (max - min) + min;
+    
+}
+
+
+
+
